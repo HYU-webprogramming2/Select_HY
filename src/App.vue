f4079a3e5d4a113fb71ba6409f54764eb10d7df9
yerincho <forjustice9839@gmail.com>
2021-12-10 01:05:33 +0900

    feat: develop home page

--- src/App.vue
+++ src/App.vue
@@ -16,22 +16,36 @@ export default {
 <style>
 @font-face {
   font-family: 'NotoSansKR';
-  src: url('./assets/font/NotoSansKR-Bold.ttf') format('ttf');
+  src: url('./assets/fonts/NotoSansKR-Bold.ttf') format('ttf');
   font-weight: 700;
 }
 
 @font-face {
   font-family: 'NotoSansKR';
-  src: url('./assets/font/NotoSansKR-Medium.ttf') format('ttf');
+  src: url('./assets/fonts/NotoSansKR-Medium.ttf') format('ttf');
   font-weight: 400;
+}
+
+html {
+  min-width: 1440px;
+  min-height: 1024px;
+}
+
+root, body {
+  background-color: #EDF8FF;
+  margin: 0;
+  padding: 0;
+  height: 100vh;
+  width: 100%;
 }
 
 #app {
   font-family: NotoSansKR, sans-serif;
   text-align: center;
   font-weight: 700;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #000000;
+  height: 100%;
+  width: 100%;
 }
 
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,70 @@
 <template>
-  <div><p>{{ msg }}</p></div>
+  <div class="container">
+    <img :src="icon" alt="icon" width="590" height="510"/>
+    <div>
+      <div class="title">나에게 어울리는 학과는?</div>
+      <div>한양대학교 학과 중 나의 성향과 가장 잘 맞는 학과를 골라드립니다.</div>
+      <div>물론 입학과 졸업은 보장 못함.</div>
+      <button>시작하기</button>
+    </div>
+  </div>
 </template>
 
 <script>
-  export default {
-    name: "Home",
-    props: {
-      msg: String
+export default {
+  name: "Home",
+  props: {
+    msg: String
+  },
+  computed: {
+    icon() {
+      return require('../assets/svgs/hyrion.svg')
     }
   }
+}
 </script>
 
 <style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+}
+
+.title {
+  font-size: 48px;
+  font-weight: 700;
+  color: #004B86;
+  width: fit-content;
+  margin-bottom: 30px;
+}
+
+div {
+  font-size: 24px;
+  font-weight: 400;
+  margin: 10px auto auto;
+}
+
+img {
+  margin: auto auto 0 auto;
+}
+
+button {
+  background-color: #0069AA;
+  box-shadow: 0 10px 11px rgba(146, 207, 244, 0.5);
+  border-radius: 39px;
+  padding: 16px 54px;
+  font-size: 30px;
+  margin-top: 30px;
+  color: #ffffff;
+  text-align: center;
+}
+
+button:hover {
+  cursor: pointer;
+  transition: all 2s;
+  transform: scale(1.1);
+}
 
 </style>
